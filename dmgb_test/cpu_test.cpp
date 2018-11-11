2215bdb5bdce2a6e83a495800290872fc4d3dab5
Jeremias Yehdegho <j.yehdegho+github@gmail.com>
2018-11-11 18:03:22 +0100

    sra instruction

--- dmgb_test/cpu_test.cpp
+++ dmgb_test/cpu_test.cpp
@@ -615,3 +615,40 @@ SCENARIO("sla", "[cpu]") {
 		});
 	}
 }
+
+SCENARIO("sra", "[cpu]") {
+	GIVEN("cpu and mmu") {
+		cpu::cpu c{};
+		u8 m[0x10000] = {0};
+
+		rc::PROPERTY("sra",
+		[&c, &m](const u8 randv) {
+			c.a = randv;
+			c.b = randv;
+			c.c = randv;
+			c.d = randv;
+			c.e = randv;
+			c.h = randv;
+			c.l = randv;
+			m[c.hl] = randv;
+
+			c.pc = *rc::gen::suchThat(rc::gen::inRange(0, 10),
+					[&c](int x){ 
+						return x != c.hl && (x+1) != c.hl;
+					});
+
+			m[c.pc] = 0xcb;
+			const u8 cb = *rc::gen::inRange(0x28, 0x2f);
+			m[c.pc + 1] = cb;
+
+			cpu::step(c, m);
+
+			RC_ASSERT(c.cf == !!(randv & 1));
+			RC_ASSERT(c.nf == false);
+			RC_ASSERT(c.hf == false);
+
+			RC_ASSERT(reg(c, m, cb & 0x7) == static_cast<u8>(randv >> 1) | (randv & (1 << 7)));
+			RC_ASSERT(c.zf == (reg(c, m, cb & 0x7) == 0));
+		});
+	}
+}
